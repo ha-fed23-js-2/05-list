e6fae73c55bd7ed4adcc0e6848f97e4e21adc84a
David Andersson <lejonmanen@gmail.com>
2024-03-14 10:38:03 +0100

    Remove from list

--- src/components/AnimalList.jsx
+++ src/components/AnimalList.jsx
@@ -1,14 +1,27 @@
 import { forestAnimals } from '../data/data.js'
 import { useState } from 'react'
+import './AnimalList.css'
 
 const AnimalList = () => {
-	const [data /*, setData*/] = useState(forestAnimals)
+	const [data, setData] = useState(forestAnimals)
+
+	const removeAnimal = name => {
+		// Testa att funktionen får den information den behöver
+		// console.log('removeAnimal: ' + name);
+		const filteredAnimals = data.filter(animal => animal.name !== name)
+		setData(filteredAnimals)
+	}
+
 	return (
 		<ul className="animal-list">
 		{data.map(animal => (
 			<li key={animal.name}>
-				<h3> {animal.name} </h3>
+				<div className="animal-item-row">
+					<h3> {animal.name} </h3>
+					<button onClick={() => removeAnimal(animal.name)}> 🗑️ </button>
+				</div>
 				Djuret blir som högst {animal.maxHeight} och äter {animal.isHerbivore ? 'växter' : 'andra djur'}.
+
 			</li>
 		))}
 		</ul>
@@ -16,3 +29,9 @@ const AnimalList = () => {
 }
 
 export default AnimalList
+
+
+/*
+Design?
+Idéer för hur jag kan ta bort ett djur från listan
+*/
